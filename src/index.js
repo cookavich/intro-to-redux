ed3632bbb8d6671e057b2f5f2d16f4943f15db95
paulrcook <paul@lougeek.com>
2017-10-12 08:35:22 -0400

    Video 29 code. Generated the FilterLink container with connect()

--- src/index.js
+++ src/index.js
@@ -71,43 +71,33 @@ const Link = ({active, children, onClick}) => {
     );
 };
 
-class FilterLink extends Component {
-    componentDidMount() {
-        const { store } = this.context;
-        this.unsubscribe = store.subscribe(() =>
-            this.forceUpdate()
-        )
+const mapStateToLinkProps = (
+    state,
+    ownProps
+) => {
+    return {
+        active: ownProps.filter === state.visibilityFilter
     }
-
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
-
-    render() {
-        const props = this.props;
-        const { store } = this.context;
-        const state = store.getState();
-
-        return (
-            <Link
-                active={
-                    props.filter === state.visibilityFilter
-                }
-                onClick={() =>
-                    store.dispatch({
-                        type: 'SET_VISIBILITY_FILTER',
-                        filter: props.filter
-                    })
-                }
-            >
-                {props.children}
-            </Link>
-        )
-    }
-}
-FilterLink.contextTypes = {
-    store: PropTypes.object
 };
+
+const mapDispatchToLinkProps = (
+    dispatch,
+    ownProps
+) => {
+    return {
+        onClick: () => {
+            dispatch({
+                type: 'SET_VISIBILITY_FILTER',
+                filter: ownProps.filter
+            })
+        }
+    }
+};
+
+const FilterLink = connect(
+    mapStateToLinkProps,
+    mapDispatchToLinkProps
+)(Link);
 
 const Footer = () => (
     <p>
@@ -160,7 +150,6 @@ const TodoList = ({todos, onTodoClick}) => (
 let nextTodoId = 0;
 let AddTodo = ({ dispatch }) => {
     let input;
-
     return (
         <div>
             <input ref={node => {
